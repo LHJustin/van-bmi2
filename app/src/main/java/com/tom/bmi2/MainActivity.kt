77e038a417cfaea72cbd73ab4cdc7f0e1ac59580
Justin <masterofleaf@gmail.com>
2022-03-03 16:44:17 +0800

    second commit add weight and height

--- app/src/main/java/com/tom/bmi2/MainActivity.kt
+++ app/src/main/java/com/tom/bmi2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.tom.bmi2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.tom.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun bmi(view:View) {
+        println("hahaha")
+        var  weight = binding.edWeight.toString().toFloat()
+        var  height = binding.edHeight.toString().toFloat()
+        println("BMI = ${weight/(height*height)}")
     }
 }
