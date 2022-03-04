55f47e029d039eedb5d70fee2b0031e0b1d3a75f
Justin <masterofleaf@gmail.com>
2022-03-04 11:13:53 +0800

    third commit add text show

--- app/src/main/java/com/tom/bmi2/MainActivity.kt
+++ app/src/main/java/com/tom/bmi2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tom.bmi2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.tom.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +18,10 @@ class MainActivity : AppCompatActivity() {
 
     fun bmi(view:View) {
         println("hahaha")
-        var  weight = binding.edWeight.toString().toFloat()
-        var  height = binding.edHeight.toString().toFloat()
-        println("BMI = ${weight/(height*height)}")
+        var  weight = binding.edWeight.text.toString().toFloat()
+        var  height = binding.edHeight.text.toString().toFloat()
+        val  bmi: Float = weight/(height*height)
+//        println("BMI = ${weight/(height*height)}")
+        Toast.makeText(this,"Your BMI is ${bmi.toString()}",Toast.LENGTH_LONG).show()
     }
 }
