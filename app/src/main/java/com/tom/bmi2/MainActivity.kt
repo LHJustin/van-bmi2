42c7a59b76b718cc0585961d5c391060eae8ab3b
Justin <masterofleaf@gmail.com>
2022-03-08 16:33:15 +0800

    forth commit add icon and ResultActivity

--- app/src/main/java/com/tom/bmi2/MainActivity.kt
+++ app/src/main/java/com/tom/bmi2/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.tom.bmi2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tom.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +16,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.bHelp.setOnClickListener{
+            Log.d("MainActivity","Need help!")
+        }
     }
 
     fun bmi(view:View) {
-        println("hahaha")
+//        println("hahaha")
+
         var  weight = binding.edWeight.text.toString().toFloat()
         var  height = binding.edHeight.text.toString().toFloat()
-        val  bmi: Float = weight/(height*height)
+        var  bmi: Float = weight/(height*height)
 //        println("BMI = ${weight/(height*height)}")
+        Log.d("MainActivity",bmi.toString())
         Toast.makeText(this,"Your BMI is ${bmi.toString()}",Toast.LENGTH_LONG).show()
+        /*val builder = AlertDialog.Builder(this)
+        builder.setTitle("Hello")
+        builder.setMessage("Your BMI is $bmi")
+        builder.setPositiveButton("OK",null)
+        val dialog = builder.create()
+        dialog.show()*/
+        AlertDialog.Builder(this)
+            .setTitle("Messange")
+            .setMessage("Your BMI is $bmi")
+            .setPositiveButton("Ok", ){ dialog,which ->
+                binding.edWeight.setText("")
+                binding.edHeight.setText("")
+            }
+            .show()
+        binding.tvBmi.text = "Your BMI is $bmi"
+        val intent = Intent(this,ResultActivity::class.java)
+        intent.putExtra("BMI",bmi)
+        startActivity(intent)
     }
 }
